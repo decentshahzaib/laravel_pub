62f04f63b4aadc92aa26b9996c6eab4c5a6d920d
Muhammad Shahzaib <sonic.zaibi90@gmail.com>
2022-10-20 11:04:16 -0700

    3

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -11,22 +11,13 @@
         @include('common.errors')
  
         <!-- New Task Form -->
-<<<<<<< Updated upstream
         <form action="{{ route('store') }}" method="POST" class="form-horizontal">
-=======
-        <form action="/task" method="POST" class="form-horizontal">
->>>>>>> Stashed changes
             <!-- {{ csrf_field() }} -->
             @csrf
  
             <!-- Task Name -->
-<<<<<<< Updated upstream
             <div class="form-group mb-1">
                 <label for="task" class="control-label mb-3">Task</label> <span><a href="/view" class="btn btn-info">View Tasks</a></span>
-=======
-            <div class="form-group">
-                <label for="task" class="control-label">Task</label> <span><a href="/view" class="btn btn-info">View Tasks</a></span>
->>>>>>> Stashed changes
  
                 <div class="col-sm-6">
                     <input type="text" name="name" id="task-name" class="form-control">
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
-use App\Models\Task;
-
-use Illuminate\Http\Request;
-use App\Models\Task;
 use App\Http\Controllers\TaskController;
 
 /*
@@ -20,7 +14,6 @@ use App\Http\Controllers\TaskController;
 |
 */
 
-<<<<<<< Updated upstream
 Route::get('/', [TaskController::class, 'create']);
 
 Route::post('/task', [TaskController::class, 'store'])->name('store');
@@ -28,46 +21,3 @@ Route::post('/task', [TaskController::class, 'store'])->name('store');
 Route::get('/view', [TaskController::class, 'index']);
 
 Route::delete('/task/{id}', [TaskController::class, 'destroy'])->name('delete');
-=======
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
- 
-    return view('task', [
-        'tasks' => $tasks
-    ]);
-});
-
-Route::post('/task', function (Request $req) {
-    $validator = Validator::make($req->all(), [
-        'name' => 'required|max:255',
-    ]);
- 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $req->name;
-    $task->save();
- 
-    return redirect('/');
- 
-    // Create The Task...
-});
-
-Route::get('/view', function () {
-
-    $tasks = Task::all(); 
-    return view('tasks', compact('tasks'));
- 
-    // Create The Task...
-});
-
-Route::delete('/task/{id}', function ($id) {
-    Task::findOrFail($id)->delete();
- 
-    return redirect('/view');
-});
->>>>>>> Stashed changes
